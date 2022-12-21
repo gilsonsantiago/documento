91b9e97307f775b3ca444e1baa1276eeb31efdba
Gilson Santiago <gs.thiago@hotmail.com>
2022-12-21 17:14:07 +0000

    Refatorando o código.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const calcular = document.getElementById("calcular");
 const limpar = document.getElementById("limpar");
 
@@ -9,50 +8,77 @@ window.onload = function(e) {
 
 }
 
+//  limpar os campos de entrada de dados
 function limparcampos() {
+
   const peso = document.getElementById("peso");
   const altura = document.getElementById("altura");
 
   peso.value = "";
   altura.value = "";
 
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = "Digite novos valores e veja o resultado aqui...";
+
+
 }
 
-
+// calcula o IMC e faz verificação
 function calcularImc() {
 
   const peso = document.getElementById("peso");
   const altura = document.getElementById("altura");
+  const resultado = document.getElementById("resultado");
 
   let mensagem = "";
 
-  let imc = peso.value / (altura.value * altura.value);
+  if ((peso.value <= 0) || (altura.value <= 0)) {
 
-  if (imc < 20) {
+    resultado.innerHTML = "Valores inválidos. ";
 
-    mensagem = "IMC = " + imc.toFixed(2) + " - Abaixo do Peso";
+  } else {
 
-  } else if (imc >= 20 && imc < 25) {
+    let imc = peso.value / (altura.value * altura.value);
 
-    mensagem = "IMC = " + imc.toFixed(2) + " - Normal";
+    mensagem = "IMC = " + imc.toFixed(2) + tabelapeso(imc);
 
-  } else if (imc >= 25 && imc < 30) {
+    const resultado = document.getElementById("resultado");
 
-    mensagem = "IMC = " + imc.toFixed(2) + " - Excesso de peso";
-
-  } else if (imc >= 30 && imc < 35) {
-
-    mensagem = "IMC = " + imc.toFixed(2) + " - Obesidade";
-
-  } else if (imc >= 35) {
-
-    mensagem = "IMC = " + imc.toFixed(2) + " - Obesidade Mórbida";
+    resultado.innerHTML = mensagem;
 
   }
 
-  const resultado = document.getElementById("resultado");
+}
 
-  resultado.innerHTML = mensagem;
+
+// VErifica a faixa de IMC.
+function tabelapeso(imc) {
+
+  let mensagem = "";
+
+  if (imc < 20) {
+
+    mensagem = " - Abaixo do Peso";
+
+  } else if (imc >= 20 && imc < 25) {
+
+    mensagem = " - Normal";
+
+  } else if (imc >= 25 && imc < 30) {
+
+    mensagem = " - Excesso de peso";
+
+  } else if (imc >= 30 && imc < 35) {
+
+    mensagem = " - Obesidade";
+
+  } else if (imc >= 35) {
+
+    mensagem = " - Obesidade Mórbida";
+
+  }
+
+  return mensagem;
 
 }
 
